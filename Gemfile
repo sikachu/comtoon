b18bcbe3db6e32959efa38713242d88709d0da92
Prem Sichanugrist <s@sikac.hu>
2013-11-27 07:54:37 +0700

    Now working with Ruby 2.0

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'bundler'
+gem 'builder'
+gem 'sinatra'
+gem 'nokogiri'
--- comtoon.rb
+++ comtoon.rb
@@ -1,54 +1,49 @@
-require 'rubygems'
-require 'hpricot'
-require 'iconv'
-require 'time'
+require 'bundler/setup'
 require 'builder'
-require 'active_support/ordered_hash'
-include ActiveSupport
+require 'fileutils'
+require 'net/http'
+require 'nokogiri'
+require 'time'
 
 THAI_MONTHS = [nil] + %w(มกราคม กุมภาพันธ์ มีนาคม เมษายน พฤษภาคม มิถุนายน กรกฎาคม สิงหาคม กันยายน ตุลาคม พฤษจิกายน ธันวาคม)
+HOST = URI 'http://www.comtoon.com'
+COOKIE_INIT_PATH = '/v3/releaseChk.asp'
+GATEKEEPER_PATH = '/v3/release.asp'
+RELEASE_DATA_PATH = '/database/w/hl/ct_index.asp'
 
-# fetch header and stuff
-cookie = `curl -i http://www.comtoon.com/v3/releaseChk.asp`.match(/Set-Cookie: (.+); path=/)[1]
+http = Net::HTTP.new(HOST.hostname, HOST.port)
 
-# trigger stupid page
-`curl -b #{cookie} http://www.comtoon.com/v3/release.asp`
+# Fetch cookie
+response = http.get(COOKIE_INIT_PATH)
+cookie = response['Set-Cookie'].split(/;/).first
+
+# trigger gatekeeper page
+http.get(GATEKEEPER_PATH, 'Cookie' => cookie)
 
 # and then calling the actual data!
-result = `curl -b #{cookie} http://www.comtoon.com/database/w/hl/ct_index.asp`.match(/function showrelease\(\)\{x= "(.+)"; return \(x\);\}/)[1].gsub(/(&nbsp;| )+/, ' ')
-doc = Hpricot.parse(Iconv.conv('utf8', 'tis620', result))
+response = http.get(RELEASE_DATA_PATH, 'Cookie' => cookie)
+result = response.body.encode('UTF-8', 'TIS-620').match(/function showrelease\(\)\{x= "(.+)"; return \(x\);\}/)[1].gsub(/(&nbsp;| )+/, ' ')
+
+doc = Nokogiri::HTML(result)
 date = Time.parse("0:00")
 publisher = ""
-data = OrderedHash.new
+data = {}
 
-(doc/"tr").each do |tr|
+doc.css('tr').each do |tr|
   if tr[:bgcolor] == "#FFFF99"
     # encouter a new date row
-    date_match = tr.innerText.match /ที่ ([0-9]{1,2}) (.+) .+ ([0-9]{1,4})/
+    date_match = tr.text.match /ที่ ([0-9]{1,2}) (.+) .+ ([0-9]{1,4})/
     date = Time.mktime((date_match[3].to_i - 543), THAI_MONTHS.index(date_match[2]), date_match[1].to_i, 8).xmlschema[0...10]
   elsif tr[:bgcolor] == "#99CCFF"
     # encouter a new publisher row
-    publisher = tr.innerText
+    publisher = tr.text
   else
     # comic row, add a new object to hash!
-    data[date] ||= OrderedHash.new
+    data[date] ||= {}
     data[date][publisher] ||= []
-    data[date][publisher] += [tr.innerText.strip]
+    data[date][publisher] += [tr.text.strip]
   end
 end
-
-# YAML type, deprecated
-# data.each do |i| (date, publishers = i)
-#   puts ">> #{date}"
-#   output = ""
-#   publishers.each do |j| (publisher, comics = j)
-#     output += "\"#{publisher}\":\n"
-#     comics.each do |comic|
-#       output += "  - \"#{comic}\"\n"
-#     end
-#   end
-#   puts output
-# end
 
 data.each do |i| (date, publishers = i)
   output = ""
@@ -62,6 +57,7 @@ data.each do |i| (date, publishers = i)
   output += "<p>ที่มา: <a target='_blank' href='http://www.comtoon.com'>Comtoon.com</a></p>"
 
   # write to file
+  FileUtils.mkdir_p('data')
   File.open("data/#{date}.html", 'w+') {|f| f.write(output) }
 end
 
@@ -76,7 +72,7 @@ xml.rss :version => "2.0", "xmlns:atom" => "http://www.w3.org/2005/Atom" do
     xml.generator "RubyXMLBuilder"
     xml.language "th"
     xml.atom :link, :type => "application/rss+xml", :rel => "self", :href => "http://comic.dev.7republic.com/rss.xml"
-    
+
     # load files
     Dir["data/*.html"].sort{|x,y| y <=> x }.each do |filename|
       d = filename.match /([0-9]{4})-([0-9]{2})-([0-9]{2})\.html/
@@ -93,4 +89,4 @@ xml.rss :version => "2.0", "xmlns:atom" => "http://www.w3.org/2005/Atom" do
   end
 end
 
-File.open("rss.xml", 'w+') {|f| f.write(xml.target!) }
+File.open("rss.xml", 'w') {|f| f.write(xml.target!) }
